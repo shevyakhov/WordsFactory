2a282cdb22c701e5ddcc3e6bf303ef73fb09fd10
shevyakhov <shevyakhov89@gmail.com>
2022-03-01 19:57:50 +0700

    SignUpFragment.kt layout added

--- app/src/main/java/com/example/wordsfactory/fragment/IntroFragment.kt
+++ app/src/main/java/com/example/wordsfactory/fragment/IntroFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.example.wordsfactory.R
@@ -57,7 +56,8 @@ class IntroFragment : Fragment() {
     }
 
     private fun registrationFragment() {
-        /* todo */
+        parentFragmentManager.beginTransaction()
+            .add(R.id.fragmentHolder, SignUpFragment.newInstance()).commit()
     }
 
 
