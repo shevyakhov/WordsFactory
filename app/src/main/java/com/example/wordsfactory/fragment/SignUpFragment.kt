ede8fa6a0694ecb522ac51a727535f4031178ae6
shevyakhov <shevyakhov89@gmail.com>
2022-03-12 14:22:05 +0700

    navigation layout and destinations

--- app/src/main/java/com/example/wordsfactory/fragment/SignUpFragment.kt
+++ app/src/main/java/com/example/wordsfactory/fragment/SignUpFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import androidx.fragment.app.Fragment
+import com.example.wordsfactory.R
+import com.example.wordsfactory.databinding.FragmentDictionaryPlaceholderBinding
 import com.example.wordsfactory.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
@@ -27,7 +29,37 @@ class SignUpFragment : Fragment() {
     }
 
     private fun initBinding() {
+        binding.botButton.setOnClickListener {
+            with(binding) {
 
+                if (editName.text?.isNotEmpty() == true && editEmail.text?.isNotEmpty() == true && editPassword.text?.isNotEmpty() == true) {
+                    startDictionaryFragment()
+                } else {
+                    AlertFragment.newInstance(
+                        getString(R.string.warning),
+                        findEmptyFields()
+                    ).show(
+                        childFragmentManager, ""
+                    )
+                }
+            }
+        }
+    }
+
+    private fun findEmptyFields(): String {
+        var fields = getString(R.string.pleaseEnter) +"\n"
+        if (binding.editName.text?.isEmpty() == true)
+            fields += "Name "
+        if (binding.editEmail.text?.isEmpty() == true)
+            fields += "Email "
+        if (binding.editPassword.text?.isEmpty() == true)
+            fields += "Password"
+        return fields
+    }
+
+    private fun startDictionaryFragment() {
+        parentFragmentManager.beginTransaction()
+            .add(R.id.fragmentHolder, DictionaryPlaceholderFragment.newInstance()).commit()
     }
 
     companion object {
