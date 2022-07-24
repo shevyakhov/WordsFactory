f86aa952242a17d93a749ebca3e1197e6163984a
shevyakhov <shevyakhov89@gmail.com>
2022-07-25 00:29:09 +0700

    TrainingFragment.kt layout finished with animations

--- app/src/main/java/com/example/wordsfactory/ui/navigation_fragments/training/TrainingFragment.kt
+++ app/src/main/java/com/example/wordsfactory/ui/navigation_fragments/training/TrainingFragment.kt
@@ -1,5 +1,7 @@
 package com.example.wordsfactory.ui.navigation_fragments.training
 
+import android.animation.AnimatorSet
+import android.animation.ValueAnimator
 import android.graphics.Color
 import android.os.Bundle
 import android.text.Spannable
@@ -9,12 +11,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.animation.doOnEnd
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.wordsfactory.R
 import com.example.wordsfactory.databinding.FragmentTrainingBinding
+import com.example.wordsfactory.dictionary_logic.database.WordEntity
 import com.example.wordsfactory.dictionary_logic.repository.Injection
 import com.example.wordsfactory.dictionary_logic.repository.app_viewmodel.AppViewModel
 import com.example.wordsfactory.ui.navigation_fragments.training.questions.QuestionsFragment
@@ -48,18 +53,25 @@ class TrainingFragment : Fragment() {
         binding.howManyWordsText.text = wordListSize?.let { trainingViewModel.getText(it) }
 
         binding.howManyWordsText.setColouredSpan(
-            R.color.primaryOrange,
             10,
             10 + (wordListSize?.toString()?.length ?: 0)
         )
 
-
         binding.startBtn.setOnClickListener {
-            // (activity as MainActivity).trainingFragment(this)
-            findNavController().navigate(
-                R.id.action_trainingFragment_to_questionsFragment,
-                bundleOf(QuestionsFragment.WORDS to wordList)
-            )
+            if (wordListSize != 0) {
+                val pairList = listOf(
+                    AnimationPair(getString(R.string.orange), getString(R.string._5)),
+                    AnimationPair(getString(R.string.blue), getString(R.string._4)),
+                    AnimationPair(getString(R.string.green), getString(R.string._3)),
+                    AnimationPair(getString(R.string.yellow), getString(R.string._2)),
+                    AnimationPair(getString(R.string.red), getString(R.string._1)),
+                    AnimationPair(getString(R.string.orange), getString(R.string._GO)),
+                )
+                timerAnimation(pairList, wordList)
+
+            } else
+                Toast.makeText(context, "add more words to start training", Toast.LENGTH_SHORT)
+                    .show()
         }
 
     }
@@ -69,7 +81,7 @@ class TrainingFragment : Fragment() {
         _binding = null
     }
 
-    private fun TextView.setColouredSpan(color: Int, starts: Int, ends: Int) {
+    private fun TextView.setColouredSpan(starts: Int, ends: Int) {
         val spannableString = SpannableString(text)
         try {
             spannableString.setSpan(
@@ -83,4 +95,53 @@ class TrainingFragment : Fragment() {
             println("error")
         }
     }
+
+    private fun timerAnimation(list: List<AnimationPair>, wordList: List<WordEntity>?) {
+        binding.timer.visibility = View.VISIBLE
+
+        val animator = ValueAnimator.ofArgb(
+            requireContext().getColor(R.color.primaryOrange),
+            requireContext().getColor(R.color.animBlue),
+            requireContext().getColor(R.color.animYellow),
+            requireContext().getColor(R.color.animGreen),
+            requireContext().getColor(R.color.animRed),
+            requireContext().getColor(R.color.primaryOrange)
+        ).apply {
+            duration = 6000
+            addUpdateListener {
+                val color = it.animatedValue as Int
+                binding.progressBar.setIndicatorColor(color)
+                binding.countDown.setTextColor(color)
+            }
+        }
+
+        val animatorProgress = ValueAnimator.ofInt(0, 100).apply {
+            duration = 6000
+            addUpdateListener {
+                binding.progressBar.progress = it.animatedValue as Int
+                val ind = it.animatedValue as Int / 20
+                if (ind in 0..5) {
+                    binding.countDown.text = list[ind].text
+                }
+            }
+        }
+
+        val animatorDelayer = ValueAnimator.ofInt(0, 100).apply {
+            duration = 500
+        }
+
+        val animatorSet = AnimatorSet().apply {
+            play(animator).with(animatorProgress).before(animatorDelayer)
+            start()
+        }
+
+        animatorSet.doOnEnd {
+            findNavController().navigate(
+                R.id.action_trainingFragment_to_questionsFragment,
+                bundleOf(QuestionsFragment.WORDS to wordList)
+            )
+        }
+    }
+
+    data class AnimationPair(val color: String, val text: String)
 }
